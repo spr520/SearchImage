14d9e5e9f35394e3079bc9dae16f5ec5e3803eb1
Spr <spr520@gmail.com>
2016-07-12 21:33:19 +0800

    ----- create project -----

---
+++ app/src/main/java/com/spr/whoshomework/ImageSearchActivity.java
@@ -0,0 +1,16 @@
+package com.spr.whoshomework;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ImageSearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image_search);
+
+        // https://pixabay.com/api/?key=2906241-7999c5df50f7c9de92cce050c&q=yellow+flowers&image_type=photo
+
+    }
+}
