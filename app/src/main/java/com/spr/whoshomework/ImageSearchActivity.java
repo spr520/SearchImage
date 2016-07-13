6ade0631d6ed0ba0a7d16dd914a5bb552fde660a
Spr <spr520@gmail.com>
2016-07-13 21:24:22 +0800

    append input to Url

--- app/src/main/java/com/spr/whoshomework/ImageSearchActivity.java
+++ app/src/main/java/com/spr/whoshomework/ImageSearchActivity.java
@@ -7,17 +7,19 @@ import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.SearchView;
+import android.util.Log;
 import android.view.Menu;
 
 public class ImageSearchActivity extends AppCompatActivity {
     private Menu mMenu;
+    private String TAG = "Spr";
+
+    private String PIXABAYURL = "https://pixabay.com/api/?key=2906241-7999c5df50f7c9de92cce050c&q=%s&image_type=photo";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image_search);
-
-        // https://pixabay.com/api/?key=2906241-7999c5df50f7c9de92cce050c&q=yellow+flowers&image_type=photo
 
     }
 
@@ -31,17 +33,17 @@ public class ImageSearchActivity extends AppCompatActivity {
 
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
-
             SearchManager manager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
-
             SearchView search = (SearchView) menu.findItem(R.id.search).getActionView();
-
             search.setSearchableInfo(manager.getSearchableInfo(getComponentName()));
-
             search.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-
                 @Override
                 public boolean onQueryTextSubmit(String s) {
+                    s = s.replace(" " , "+");
+                    Log.d(TAG, "keyword = " + s);
+                    String output = String.format(PIXABAYURL, s);
+
+                    Log.d(TAG,"submit url  =  " + output);
                     return false;
                 }
 
