608570b22f0edfd19544cb585e3b72baf8ffa465
Spr <spr520@gmail.com>
2016-07-16 15:54:41 +0800

    retrofit(3) -- finish query by keyword

--- app/src/main/java/com/spr/whoshomework/ImageSearchActivity.java
+++ app/src/main/java/com/spr/whoshomework/ImageSearchActivity.java
@@ -10,16 +10,22 @@ import android.support.v7.widget.SearchView;
 import android.util.Log;
 import android.view.Menu;
 
+import java.util.List;
+
 import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
-import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Query;
 
 public class ImageSearchActivity extends AppCompatActivity {
     private Menu mMenu;
     private String TAG = "Spr";
 
-    private String PIXABAYURL = "https://pixabay.com/api/?key=2906241-7999c5df50f7c9de92cce050c&q=%s&image_type=photo";
+    private String BASE_URL = "https://pixabay.com/";
+    // sample https://pixabay.com/api/?key=2906241-7999c5df50f7c9de92cce050c&q=cat&image_type=photo
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,13 +45,10 @@ public class ImageSearchActivity extends AppCompatActivity {
             search.setSearchableInfo(manager.getSearchableInfo(getComponentName()));
             search.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
                 @Override
-                public boolean onQueryTextSubmit(String s) {
-                    s = s.replace(" " , "+");
-                    Log.d(TAG, "keyword = " + s);
-                    String output = String.format(PIXABAYURL, s);
-
-                    Log.d(TAG,"submit url  =  " + output);
-                    getJson(output);
+                public boolean onQueryTextSubmit(String text) {
+                    text = text.replace(" " , "+");
+                    Log.d(TAG, "keyword = " + text);
+                    getJson(text);
                     return false;
                 }
 
@@ -62,31 +65,68 @@ public class ImageSearchActivity extends AppCompatActivity {
 
     }
 
-    private void getJson(String url) {
+    private void getJson(String keyword) {
         Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl(url)
+                .baseUrl(BASE_URL)
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
+
+        PixabayService service = retrofit.create(PixabayService.class);
+        Call<Pixabay> call = service.pixabays(keyword);
+
+        call.enqueue(new Callback<Pixabay>() {
+            @Override
+            public void onResponse(Call<Pixabay> call, Response<Pixabay> response) {
+                Pixabay pixabay= response.body();
+                int responseCode = response.code();
+                Log.d(TAG,"response code = " + responseCode);
+                if(responseCode == 200) {
+                    List<Pixabay.PixabayItem> items = pixabay.hits;
+                    for(Pixabay.PixabayItem item:items) {
+                        Log.d(TAG,"item " + item.webformatURL);
+                    }
+
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Pixabay> call, Throwable t) {
+                Log.d(TAG,"get data fail");
+            }
+        });
+
+
+    }
+
+
+    public interface PixabayService {
+        @POST("api/?key=2906241-7999c5df50f7c9de92cce050c&image_type=photo")
+        Call<Pixabay> pixabays(
+                @Query("q") String keyword);
     }
 
     public class Pixabay {
-        int total;
-        int totalHits;
-        PixabayItem hits;
+
+        String totalHits;
+        String total;
+
+        @Override
+        public String toString() {
+            return(totalHits);
+        }
+
+        List<PixabayItem> hits;
 
 
         public class PixabayItem {
-            String previewURL;
-            String userImageURL;
+            String webformatURL;
+            int webformatWidth;
+            int webformatHeight;
 
         }
-    }
 
-    public interface PhoneService {
-        @GET("")
-        Call<Pixabay> getResult();
-    }
 
+    }
 
 
 }
